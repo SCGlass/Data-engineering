8e7b8fe39edaf1751ae74cb96a25c976b26b3064
Sam Glass <sam.charles.glass@gmail.com>
2023-05-31 11:22:41 +0200

    Taking a break

---
+++ Exercise1-1/countdown.py
@@ -0,0 +1,11 @@
+import datetime
+
+events = [
+    {"name": "summer_break", "date":"2023-06-09 15:00"},
+    {"name": "lia_start", "date":"2023-09-25 8:00"},
+    {"name" : "christmas", "date":"2023-12-24"},
+    {"name" : "bellas_birthday", "date":"2023-12-07"},
+    {"name" : "new_year", "date" : "2024-01-01"},
+    {"name" : "graduation_party", "date" : "2024-06-09 16:30"}
+    ]
+
