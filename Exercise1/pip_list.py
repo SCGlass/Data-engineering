e7e95e4934fa267b314a353bc3fb007ae89f18a4
Sam Glass <sam.charles.glass@gmail.com>
2023-05-31 11:23:06 +0200

    adding pip list.py

--- Exercise1/pip_list.py
+++ Exercise1/pip_list.py
@@ -2,8 +2,11 @@
 #%%
 import subprocess
 from pathlib import Path
+import sys
 
 path_to_script = Path(__file__).parent/"pip_list.sh"
 
-subprocess.call(["bash", path_to_script])
+subprocess.call(["bash", path_to_script.as_posix()])
+
+print(f"Python version: {sys.version}")
 # %%
