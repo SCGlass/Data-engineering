89d9fdd342aba25eb7ae006b9c8b01a60cbf9831
Sam Glass <sam.charles.glass@gmail.com>
2023-08-16 21:08:45 +0200

    lecture 6 29:34

---
+++ Code-alongs/Lec4_airflow_ELT/include/queue_time/extract.py
@@ -0,0 +1,20 @@
+from airflow.operators.python import PythonOperator
+import requests, pytz
+
+from datetime import datetime
+
+theme_parks = {"liseberg": 11, "asterix" : 9}
+
+#TODO: for reader explore timezones yourself to see how it works
+stockholm_timezone = pytz.timezone("Europe/Stockholm")
+
+def _extract_queue_time(theme_park):
+    pass
+
+
+@task_group(group_id = "extract_liseberg")
+def extract_queue_time():
+    extract_queue_time = PythonOperator(
+      task_id = "extract_queue_time",
+      python_callable= _extract_queue_time  
+    )
