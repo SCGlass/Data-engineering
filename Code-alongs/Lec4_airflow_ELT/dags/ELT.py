545646307ba6fedf62769cc597e3a2f62c1c9a28
Sam Glass <sam.charles.glass@gmail.com>
2023-08-16 19:58:11 +0200

    ELT module setup

--- Code-alongs/Lec4_airflow_ELT/dags/ELT.py
+++ Code-alongs/Lec4_airflow_ELT/dags/ELT.py
@@ -2,9 +2,9 @@ from airflow.decorators import dag, task_group, task
 from datetime import datetime
 from include.setup import setup_directories
 
-@dag(dag_id = "que_time_ELT", start_date=datetime(2023,8,16))
+@dag(dag_id = "queue_time_ELT", start_date=datetime(2023,8,16))
 def queue_time_ELT():
-    setup = setup_directories
+    setup = setup_directories()
 
     setup
 
--- Code-alongs/Lec4_airflow_ELT/include/setup.py
+++ Code-alongs/Lec4_airflow_ELT/include/setup.py
@@ -4,9 +4,18 @@ from airflow.decorators import task_group
 
 data_path = Path(__file__).parents[1] / "data"
 datalake_path = data_path / "data_lake"
-datalake_warehouse_path = data_path / "data_warehouse"
+data_warehouse_path = data_path / "data_warehouse"
 
 @task_group(group_id="setup_data_directories")
 def setup_directories():
-    pass
+    create_directories = BashOperator(
+        task_id="create_directories", 
+        bash_command=f"mkdir -p {datalake_path} {data_warehouse_path}",
+        )
+    
+    success_setup = BashOperator(
+        task_id="setup_success",
+        bash_command= "echo 'setup data directories successful'"
+    )
 
+    create_directories >> success_setup
