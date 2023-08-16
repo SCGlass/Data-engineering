de0a28b4f6bfe73d24f624c1257d764182864d83
Sam Glass <sam.charles.glass@gmail.com>
2023-08-16 17:38:13 +0200

    code alongs setup ELT

---
+++ Code-alongs/Lec4_airflow_ELT/include/setup.py
@@ -0,0 +1,12 @@
+from airflow.operators.bash import BashOperator
+from pathlib import Path
+from airflow.decorators import task_group
+
+data_path = Path(__file__).parents[1] / "data"
+datalake_path = data_path / "data_lake"
+datalake_warehouse_path = data_path / "data_warehouse"
+
+@task_group(group_id="setup_data_directories")
+def setup_directories():
+    pass
+
